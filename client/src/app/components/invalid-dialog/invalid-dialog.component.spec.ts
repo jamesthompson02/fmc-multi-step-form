4077e0a817e1107fab52785802652428fb59b42b
jamesthompson02 <jamesthompsongithub@outlook.com>
2024-09-26 21:05:55 +0100

    Have written unit tests for some more components.

--- client/src/app/components/invalid-dialog/invalid-dialog.component.spec.ts
+++ client/src/app/components/invalid-dialog/invalid-dialog.component.spec.ts
@@ -1,23 +1,48 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import {
+  byRole,
+  byTestId,
+  createComponentFactory,
+  mockProvider,
+  Spectator,
+} from '@ngneat/spectator/jest';
 import { InvalidDialogComponent } from './invalid-dialog.component';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MockPipe } from 'ng-mocks';
+import { CapitalPipe } from '../../pipes/capital/capital.pipe';
 
 describe('InvalidDialogComponent', () => {
-  let component: InvalidDialogComponent;
-  let fixture: ComponentFixture<InvalidDialogComponent>;
+  let spectator: Spectator<InvalidDialogComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [InvalidDialogComponent]
-    })
-    .compileComponents();
+  const createComponent = createComponentFactory({
+    component: InvalidDialogComponent,
+    imports: [CapitalPipe],
+    providers: [
+      {
+        provide: MAT_DIALOG_DATA,
+        useValue: { invalid: ['name', 'email', 'phone'] },
+      },
+    ],
+  });
 
-    fixture = TestBed.createComponent(InvalidDialogComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  beforeEach(() => {
+    spectator = createComponent();
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(spectator).toBeTruthy();
+  });
+
+  it('should render the MAT_DIALOG_DATA in the UI', () => {
+    const invalidFormControls = spectator.queryAll('li');
+    const invalidFormControlsTextContent = invalidFormControls.map(
+      (invalid) => invalid.textContent
+    );
+
+    expect(invalidFormControls).toHaveLength(3);
+    expect(invalidFormControlsTextContent).toEqual([
+      'Name.',
+      'Email.',
+      'Phone.',
+    ]);
   });
 });
--- client/src/app/components/summary/summary.component.spec.ts
+++ client/src/app/components/summary/summary.component.spec.ts
@@ -2,6 +2,7 @@ import { subscriptionAddOns, subscriptionPlans } from '../../globals/globals';
 import { SelectedIndexService } from '../../services/selectedIndex/selected-index.service';
 import { SummaryComponent } from './summary.component';
 import {
+  byTestId,
   createComponentFactory,
   mockProvider,
   Spectator,
@@ -28,5 +29,53 @@ describe('SummaryComponent', () => {
 
   it('should create', () => {
     expect(spectator).toBeTruthy();
+  });
+
+  it('should render data from the inputs appropriately in the UI when yearly input is true', () => {
+    const planName = spectator.query(byTestId('plan-name-yearly'));
+    const planPriceYearly = spectator.query(byTestId('plan-price-yearly'));
+    const totalPrice = spectator.query(byTestId('total-price'));
+
+    expect(planName?.textContent?.trim()).toBe('Arcade (Yearly)');
+    expect(planPriceYearly?.textContent?.trim()).toBe('$90/year');
+    expect(totalPrice?.textContent?.trim()).toBe('$90/year');
+  });
+
+  it('should successfully assign values to the selectedPlanOption and totalPrice properties', () => {
+    const calculateTotalPriceFn = jest.spyOn(
+      spectator.component,
+      'calculateTotalPrice'
+    );
+    const extractNumberFromStringFn = jest.spyOn(
+      spectator.component,
+      'extractNumberFromString'
+    );
+    const selectedPlanOption = spectator.component.selectedPlanOption;
+
+    spectator.component.ngOnInit();
+    spectator.detectChanges();
+
+    expect(calculateTotalPriceFn).toHaveLastReturnedWith('$90/year');
+    expect(extractNumberFromStringFn).toHaveBeenCalledTimes(1);
+    expect(extractNumberFromStringFn).toHaveLastReturnedWith('90');
+    expect(selectedPlanOption).toEqual({
+      value: 'Arcade',
+      cardImg: 'icon-arcade.svg',
+      cardImgAlt: 'Icon of old joystick controller.',
+      price: { monthly: '$9/month', yearly: '$90/year' },
+    }); //use toEqual rather than toBe because toBe should only be used with primitive types
+  });
+
+  it('should redirect user to step 2 of form when change is clicked', () => {
+    const change = spectator.query(byTestId('change')) as HTMLSpanElement;
+    const changePlanFn = jest.spyOn(spectator.component, 'changePlan');
+    const updateSelectedIndexFn =
+      spectator.inject(SelectedIndexService).updateSelectedIndex;
+
+    spectator.click(change);
+    spectator.detectChanges();
+
+    expect(changePlanFn).toHaveBeenCalled();
+    expect(updateSelectedIndexFn).toHaveBeenCalledWith(1);
   });
 });
